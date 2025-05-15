baa2ebdf0f39dac2f6b4192e667bb24a02e40ee7
Espy <espy.jacob@gmail.com>
2025-05-15 03:58:25 -0700

    Version 1.0.0 for 1.21.5

---
+++ src/main/java/espy/heartcore/config/ModConfig.java
@@ -0,0 +1,41 @@
+package espy.heartcore.config;
+
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Config(name = "heartcore")
+public class ModConfig implements ConfigData {
+
+
+    @ConfigEntry.Gui.CollapsibleObject
+    public Healing healingConfig = new Healing();
+
+    @ConfigEntry.Gui.CollapsibleObject
+    public Respawning respawningConfig = new Respawning();
+
+
+    public static class Healing {
+        public int maxHearts = 10;
+        public int minHearts = 1;
+        public int respawnCost = 1;
+        public boolean regainHearts = true;
+        public List<String> healingItems = new ArrayList<>(List.of("minecraft:enchanted_golden_apple"));
+    }
+
+    public static class Respawning {
+        public boolean randomRespawn = false;
+        public int maxRadius = 25000;
+        public int minRadius = 20000;
+    }
+
+    @SuppressWarnings("unused")
+    public ModConfig() {}
+
+    public ModConfig(int minHearts) {
+        this.healingConfig.minHearts = minHearts;
+    }
+}
---
+++ src/main/java/espy/heartcore/event/ItemFinishedEvents.java
@@ -0,0 +1,47 @@
+package espy.heartcore.event;
+
+import espy.heartcore.Heartcore;
+import espy.heartcore.util.HeartcoreManager;
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.NbtComponent;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.World;
+
+public interface ItemFinishedEvents {
+    Event<ItemFinishedEvents> EVENT = EventFactory.createArrayBacked(ItemFinishedEvents.class,
+            (listeners) -> (player, world, stack) -> {
+                for (ItemFinishedEvents listener : listeners) {
+                    listener.onFinish(player, world, stack);
+                }
+            });
+
+    void onFinish(PlayerEntity player, World world, ItemStack stack);
+
+    class DefaultHandler {
+        public static void register() {
+            EVENT.register((player, world, stack) -> {
+                if (!(player instanceof ServerPlayerEntity serverPlayer)) return;
+
+                boolean inConfig = Heartcore.CONFIG.healingConfig.healingItems.contains(stack.getItem().toString());
+                boolean hasHeartTag = hasAddHeartTag(stack);
+
+                if (inConfig || hasHeartTag) {
+                    HeartcoreManager.addHeart(serverPlayer);
+                }
+            });
+        }
+    }
+
+    private static boolean hasAddHeartTag(ItemStack stack) {
+        NbtComponent component = stack.get(DataComponentTypes.CUSTOM_DATA);
+        if (component == null) return false;
+
+        NbtCompound nbt = component.copyNbt();
+        return nbt.contains("heartcore:add_heart") && nbt.getBoolean("heartcore:add_heart", false);
+    }
+}

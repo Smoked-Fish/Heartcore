ee7cbaf4d3ad378a3d11e58c74ad3a2cfea01050
Espy <espy.jacob@gmail.com>
2025-05-20 08:10:24 -0700

    Version 1.0.1 for 1.21.3 * Added /heartcore giftheart command for multiplayer * Added /heartcore config commands for server operators * Fixed a bug causing servers accessing a client-only mixin * Fixed a bug where minHearts wasn't syncing with the client * Added heartsPerHealingItem config * Added support for half hearts * Renamed the NBT tag from "heartcore:add_heart" to "heartcore:heart_modifier" * "heartcore:heart_modifier" now accepts an integer for adding/removing a custom amount of hearts

--- src/main/java/espy/heartcore/event/HeartcoreEventRegistry.java
+++ src/main/java/espy/heartcore/event/HeartcoreEventRegistry.java
@@ -1,10 +1,12 @@
 package espy.heartcore.event;
 
 import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 
 public class HeartcoreEventRegistry {
     public static void register() {
         ServerPlayerEvents.AFTER_RESPAWN.register(RespawnEvents::onPlayerRespawn);
-        ItemFinishedEvents.DefaultHandler.register();
+        ServerPlayConnectionEvents.JOIN.register(JoinEvents::onPlayerJoin);
+        ItemFinishedEvents.EVENT.register(ItemFinishedEvents::onItemFinished);
     }
 }
